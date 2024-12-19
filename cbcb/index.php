9690e189237f92b813570d7723e6bf34c65367b6
dinifa <dinifa26@gmail.com>
2024-12-20 02:48:19 +0700

    Update index.php

--- cbcb/index.php
+++ cbcb/index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UTS | TI-2C</title>
     <link href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" rel="stylesheet">
 </head>
+
 <body>
     <nav class="navbar is-light" role="navigation" aria-label="main navigation">
         <div class="container">
             <div class="navbar-brand">
                 <a class="navbar-item" href="index.php">Perpus</a>
-            </div>
-            <div class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item" href="?page=buku">Data Buku</a>
-                    <a class="navbar-item" href="?page=buku-resep-masakan">Buku Resep Masakan</a>
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">Menu</a>
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item" href="?page=buku">Data Buku</a>
+                        <a class="navbar-item" href="?page=buku-resep-masakan">Buku Resep Masakan</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -78,4 +81,5 @@
     </div>
 
 </body>
+
 </html>
